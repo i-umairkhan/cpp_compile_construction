732c3d6124af990c5ba55fe43f19c27254851348
Umair Khan <81556052+umair2607@users.noreply.github.com>
2022-10-30 23:59:35 +0500

    Update compiler.cpp

--- compiler.cpp
+++ compiler.cpp
@@ -1,230 +1,437 @@
 #include <fstream>
 #include <iostream>
 using namespace std;
-int main() {
-  fstream myfile;
-  myfile.open("text.txt", ios::in);
-  if (!myfile) {
-    cout << "No such file";
-  } else {
-    char ch;
-    string keyword = "";
-    string digit = "";
-    while (!myfile.eof()) {
+
+fstream myfile;
+char ch;
+
+enum TokenType {
+  KeywordToken,
+  VariableToken,
+  DigitToken,
+  SymbaolTokan,
+};
+
+struct Token {
+  TokenType type;
+  string val;
+};
+Token Lexical() {
+  Token t;
+  string keyword = "";
+  string digit = "";
+  if (isalpha(ch)) {
+    keyword += ch;
+    while (true) {
       myfile.get(ch);
-      if (myfile.eof()) {
-        break;
-      } else if (isalpha(ch)) {
+      if (isalpha(ch)) {
         keyword += ch;
-        while (true) {
-          myfile.get(ch);
-          if (isalpha(ch)) {
-            keyword += ch;
-          } else {
-            myfile.putback(ch);
-            break;
-          }
-        }
-        if (keyword == "int" || keyword == "using" || keyword == "auto" ||
-            keyword == "double" || keyword == "int" || keyword == "struct" ||
-            keyword == "break" || keyword == "else" || keyword == "long" ||
-            keyword == "switch" || keyword == "case" || keyword == "enum" ||
-            keyword == "register" || keyword == "typedef" ||
-            keyword == "char" || keyword == "extern" || keyword == "return" ||
-            keyword == "union" || keyword == "return" ||
-            keyword == "continue" || keyword == "for" || keyword == "signed" ||
-            keyword == "void" || keyword == "do" || keyword == "if" ||
-            keyword == "while" || keyword == "default" || keyword == "goto" ||
-            keyword == "sizeof" || keyword == "volatile" ||
-            keyword == "const" || keyword == "float" || keyword == "short" ||
-            keyword == "unsigned" || keyword == "Public" ||
-            keyword == "Private" || keyword == "namespace" ||
-            keyword == "include" || keyword == "iostream" || keyword == "std" ||
-            keyword == "main" || keyword == "fstream"
-
-        ) {
-          cout << "Keyword" << endl;
-          keyword = "";
-        } else {
-          cout << "Variable" << endl;
-          keyword = "";
-        }
-      } else if (isdigit(ch)) {
-        digit += ch;
-        while (true) {
-          myfile.get(ch);
-          if (isdigit(ch)) {
-            digit += ch;
-          } else {
-            myfile.putback(ch);
-            break;
-          }
-        }
-        cout << "Number" << endl;
-      } else if (ch == '#') {
-        cout << "Hash" << endl;
-      } else if (ch == '!') {
-        cout << "Not" << endl;
-      } else if (ch == '.') {
-        cout << "Dot" << endl;
-      } else if (ch == '"') {
-        cout << "Double Qoute" << endl;
-      } else if (ch == '/') {
-        myfile.get(ch);
-        if (ch == '/') {
-          cout << "Comment" << endl;
-        } else if (ch == '=') {
-          cout << "FarwardSlashEqual" << endl;
-        } else {
-          myfile.putback(ch);
-          cout << "FarwardSlash" << endl;
-        }
-
-      } else if (ch == '%') {
-        myfile.get(ch);
-        if (ch == '=') {
-          cout << "PercentEqual" << endl;
-        } else {
-          myfile.putback(ch);
-          cout << "Percentage" << endl;
-        }
-
-      } else if (ch == '|') {
-        myfile.get(ch);
-        if (ch == '=') {
-          cout << "PipeEqual" << endl;
-        } else {
-          myfile.putback(ch);
-          cout << "Pipe" << endl;
-        }
-
-      }
-
-      else if (ch == '&') {
-        myfile.get(ch);
-        if (ch == '=') {
-          cout << "AndEqual" << endl;
-        } else {
-          myfile.putback(ch);
-          cout << "And" << endl;
-        }
-
-      } else if (ch == '^') {
-        myfile.get(ch);
-        if (ch == '=') {
-          cout << "PowerEqual" << endl;
-        } else {
-          myfile.putback(ch);
-          cout << "Power" << endl;
-        }
-
-      }
-
-      else if (ch == '*') {
-        myfile.get(ch);
-        if (ch == '=') {
-          cout << "MultipyEqual" << endl;
-        } else {
-          myfile.putback(ch);
-          cout << "Multiply" << endl;
-        }
-      } else if (ch == '-') {
-        myfile.get(ch);
-        if (ch == '-') {
-          cout << "Decrement" << endl;
-        } else if (ch == '=') {
-          cout << "MinusEqual" << endl;
-        } else {
-          myfile.putback(ch);
-          cout << "Minus" << endl;
-        }
-      }
-
-      else if (ch == '+') {
-        myfile.get(ch);
-        if (ch == '+') {
-          cout << "Increment" << endl;
-        } else if (ch == '=') {
-          cout << "PlusEqual" << endl;
-        } else {
-          myfile.putback(ch);
-          cout << "Plus" << endl;
-        }
-
-      } else if (ch == ':') {
-        myfile.get(ch);
-        if (ch == ':') {
-          cout << "Double Qoulan" << endl;
-        } else {
-          myfile.putback(ch);
-          cout << "Colan" << endl;
-        }
-      } else if (ch == ' ') {
-        cout << "Space" << endl;
-      } else if (ch == '\n') {
-        cout << "NewLine" << endl;
-      } else if (ch == '\t') {
-        cout << "Tab" << endl;
-      } else if (ch == '=') {
-        cout << "Equal" << endl;
-      } else if (ch == ';') {
-        cout << "Semicolan" << endl;
-      } else if (ch == '?') {
-        cout << "Question Mark" << endl;
-      } else if (ch == '[') {
-        cout << "Farward Squar Bracket" << endl;
-      } else if (ch == ']') {
-        cout << "Backward Squar Bracket" << endl;
-      } else if (ch == ',') {
-        cout << "Qomma" << endl;
-      } else if (ch == '~') {
-        cout << "Tilda" << endl;
-      } else if (ch == '\\') {
-        cout << "Backward Curly Bracket" << endl;
-      } else if (ch == '_') {
-        cout << "Under Score" << endl;
-      } else if (ch == '$') {
-        cout << "Dollar" << endl;
-      } else if (ch == '\'') {
-        cout << "Single Qoute" << endl;
-      } else if (ch == '(') {
-        cout << "Farward Small Bracket" << endl;
-      } else if (ch == ')') {
-        cout << "Backward Small bracket" << endl;
-      } else if (ch == '{') {
-        cout << "Farward Curly Bracket" << endl;
-      } else if (ch == '}') {
-        cout << "Backward Curly bracket" << endl;
-      } else if (ch == '>') {
-        myfile.get(ch);
-        if (ch == '>') {
-          myfile.get(ch);
-          if (ch == '=') {
-            cout << ">>=" << endl;
-          } else {
-            myfile.putback(ch);
-            cout << "cin" << endl;
-          }
-        } else {
-          myfile.putback(ch);
-          cout << "Grater" << endl;
-        }
-      } else if (ch == '<') {
-        myfile.get(ch);
-        if (ch == '<') {
-          myfile.get(ch);
-          if (ch == '=') {
-            cout << "<<=" << endl;
-          } else {
-            myfile.putback(ch);
-            cout << "cout" << endl;
-          }
-        } else {
-          myfile.putback(ch);
-          cout << "less" << endl;
-        }
+      } else {
+        myfile.putback(ch);
+        break;
       }
     }
+    if (keyword == "int" || keyword == "using" || keyword == "auto" ||
+        keyword == "double" || keyword == "int" || keyword == "struct" ||
+        keyword == "break" || keyword == "else" || keyword == "long" ||
+        keyword == "switch" || keyword == "case" || keyword == "enum" ||
+        keyword == "register" || keyword == "typedef" || keyword == "char" ||
+        keyword == "extern" || keyword == "return" || keyword == "union" ||
+        keyword == "return" || keyword == "continue" || keyword == "for" ||
+        keyword == "signed" || keyword == "void" || keyword == "do" ||
+        keyword == "if" || keyword == "while" || keyword == "default" ||
+        keyword == "goto" || keyword == "sizeof" || keyword == "volatile" ||
+        keyword == "const" || keyword == "float" || keyword == "short" ||
+        keyword == "unsigned" || keyword == "Public" || keyword == "Private" ||
+        keyword == "namespace" || keyword == "include" ||
+        keyword == "iostream" || keyword == "std" || keyword == "main" ||
+        keyword == "fstream"
+
+    ) {
+      // cout << "Keyword" << endl;
+      t.type = KeywordToken;
+      t.val = keyword;
+      return t;
+      keyword = "";
+    } else {
+      // cout << "Variable" << endl;
+      t.type = VariableToken;
+      t.val = keyword;
+      return t;
+      keyword = "";
+    }
+  } else if (isdigit(ch)) {
+    digit += ch;
+    while (true) {
+      myfile.get(ch);
+      if (isdigit(ch)) {
+        digit += ch;
+      } else {
+        myfile.putback(ch);
+        break;
+      }
+    }
+    // cout << "Number" << endl;
+    t.type = DigitToken;
+    t.val = digit;
+    return t;
+
+  } else if (ch == '#') {
+    // cout << "Hash" << endl;
+    t.type = SymbaolTokan;
+    t.val = "#";
+    return t;
+
+  } else if (ch == '!') {
+    // cout << "Not" << endl;
+    t.type = SymbaolTokan;
+    t.val = "!";
+    return t;
+  } else if (ch == '.') {
+    // cout << "Dot" << endl;
+    t.type = SymbaolTokan;
+    t.val = ".";
+    return t;
+  } else if (ch == '"') {
+    // cout << "Double Qoute" << endl;
+    t.type = SymbaolTokan;
+    t.val = "\"";
+    return t;
+  } else if (ch == '/') {
+    myfile.get(ch);
+    if (ch == '/') {
+      // cout << "Comment" << endl;
+      t.type = SymbaolTokan;
+      t.val = "//";
+      return t;
+    } else if (ch == '=') {
+      // cout << "FarwardSlashEqual" << endl;
+      t.type = SymbaolTokan;
+      t.val = "/=";
+      return t;
+    } else {
+      myfile.putback(ch);
+      // cout << "FarwardSlash" << endl;
+      t.type = VariableToken;
+      t.val = "/";
+      return t;
+    }
+
+  } else if (ch == '%') {
+    myfile.get(ch);
+    if (ch == '=') {
+      // cout << "PercentEqual" << endl;
+      t.type = SymbaolTokan;
+      t.val = "%=";
+      return t;
+    } else {
+      myfile.putback(ch);
+      // cout << "Percentage" << endl;
+      t.type = SymbaolTokan;
+      t.val = "%";
+      return t;
+    }
+
+  } else if (ch == '|') {
+    myfile.get(ch);
+    if (ch == '=') {
+      // cout << "PipeEqual" << endl;
+      t.type = SymbaolTokan;
+      t.val = "|=";
+      return t;
+    } else {
+      myfile.putback(ch);
+      // cout << "Pipe" << endl;
+      t.type = SymbaolTokan;
+      t.val = "|";
+      return t;
+    }
+
   }
-  myfile.close();
+
+  else if (ch == '&') {
+    myfile.get(ch);
+    if (ch == '=') {
+      // cout << "AndEqual" << endl;
+      t.type = SymbaolTokan;
+      t.val = "&=";
+      return t;
+    } else {
+      myfile.putback(ch);
+      // cout << "And" << endl;
+      t.type = SymbaolTokan;
+      t.val = "&";
+      return t;
+    }
+
+  } else if (ch == '^') {
+    myfile.get(ch);
+    if (ch == '=') {
+      // cout << "PowerEqual" << endl;
+      t.type = SymbaolTokan;
+      t.val = "^=";
+      return t;
+    } else {
+      myfile.putback(ch);
+      // cout << "Power" << endl;
+      t.type = SymbaolTokan;
+      t.val = "^";
+      return t;
+    }
+
+  }
+
+  else if (ch == '*') {
+    myfile.get(ch);
+    if (ch == '=') {
+      // cout << "MultipyEqual" << endl;
+      t.type = SymbaolTokan;
+      t.val = "*=";
+      return t;
+    } else {
+      myfile.putback(ch);
+      // cout << "Multiply" << endl;
+      t.type = SymbaolTokan;
+      t.val = "*";
+      return t;
+    }
+  } else if (ch == '-') {
+    myfile.get(ch);
+    if (ch == '-') {
+      // cout << "Decrement" << endl;
+      t.type = SymbaolTokan;
+      t.val = "--";
+      return t;
+    } else if (ch == '=') {
+      // cout << "MinusEqual" << endl;
+      t.type = SymbaolTokan;
+      t.val = "-=";
+      return t;
+    } else {
+      myfile.putback(ch);
+      // cout << "Minus" << endl;
+      t.type = SymbaolTokan;
+      t.val = "-";
+      return t;
+    }
+  }
+
+  else if (ch == '+') {
+    myfile.get(ch);
+    if (ch == '+') {
+      // cout << "Increment" << endl;
+      t.type = SymbaolTokan;
+      t.val = "++";
+      return t;
+    } else if (ch == '=') {
+      // cout << "PlusEqual" << endl;
+      t.type = SymbaolTokan;
+      t.val = "+=";
+      return t;
+    } else {
+      myfile.putback(ch);
+      // cout << "Plus" << endl;
+      t.type = SymbaolTokan;
+      t.val = "+";
+      return t;
+    }
+
+  } else if (ch == ':') {
+    myfile.get(ch);
+    if (ch == ':') {
+      // cout << "DoubleQoulan" << endl;
+      t.type = SymbaolTokan;
+      t.val = "::";
+      return t;
+    } else {
+      myfile.putback(ch);
+      // cout << "Colan" << endl;
+      t.type = SymbaolTokan;
+      t.val = ":";
+      return t;
+    }
+  } else if (ch == '=') {
+    myfile.get(ch);
+    if (ch == '=') {
+      // cout << "Comparison" << endl;
+      t.type = SymbaolTokan;
+      t.val = "==";
+      return t;
+    } else {
+      myfile.putback(ch);
+      // cout << "Equal" << endl;
+      t.type = SymbaolTokan;
+      t.val = "=";
+      return t;
+    }
+  }
+
+  else if (ch == ' ') {
+    // cout << "Space" << endl;
+    t.type = SymbaolTokan;
+    t.val = " ";
+    return t;
+  } else if (ch == '\n') {
+    // cout << "NewLine" << endl;
+    t.type = SymbaolTokan;
+    t.val = "\n";
+    return t;
+  } else if (ch == '\t') {
+    // cout << "Tab" << endl;
+    t.type = SymbaolTokan;
+    t.val = "\t";
+    return t;
+  } else if (ch == ';') {
+    // cout << "Semicolan" << endl;
+    t.type = SymbaolTokan;
+    t.val = ";";
+    return t;
+  } else if (ch == '?') {
+    // cout << "QuestionMark" << endl;
+    t.type = SymbaolTokan;
+    t.val = "?";
+    return t;
+  } else if (ch == '[') {
+    // cout << "FarwardSquarBracket" << endl;
+    t.type = SymbaolTokan;
+    t.val = "[";
+    return t;
+  } else if (ch == ']') {
+    // cout << "BackwardSquarBracket" << endl;
+    t.type = SymbaolTokan;
+    t.val = "]";
+    return t;
+  } else if (ch == ',') {
+    // cout << "Qomma" << endl;
+    t.type = SymbaolTokan;
+    t.val = ",";
+    return t;
+  } else if (ch == '~') {
+    // cout << "Tilda" << endl;
+    t.type = SymbaolTokan;
+    t.val = "~";
+    return t;
+  } else if (ch == '\\') {
+    // cout << "BackSlash" << endl;
+    t.type = SymbaolTokan;
+    t.val = "\\";
+    return t;
+  } else if (ch == '_') {
+    // cout << "UnderScore" << endl;
+    t.type = SymbaolTokan;
+    t.val = "_";
+    return t;
+  } else if (ch == '$') {
+    // cout << "Dollar" << endl;
+    t.type = SymbaolTokan;
+    t.val = "$";
+    return t;
+  } else if (ch == '\'') {
+    // cout << "SingleQoute" << endl;
+    t.type = SymbaolTokan;
+    t.val = "\'";
+    return t;
+  } else if (ch == '(') {
+    // cout << "FarwardSmallBracket" << endl;
+    t.type = SymbaolTokan;
+    t.val = "(";
+    return t;
+  } else if (ch == ')') {
+    // cout << "BackwardSmallBracket" << endl;
+    t.type = SymbaolTokan;
+    t.val = ")";
+    return t;
+  } else if (ch == '{') {
+    // cout << "FarwardCurlyBracket" << endl;
+    t.type = SymbaolTokan;
+    t.val = "{";
+    return t;
+  } else if (ch == '}') {
+    // cout << "BackwardCurlyBracket" << endl;
+    t.type = SymbaolTokan;
+    t.val = "}";
+    return t;
+  } else if (ch == '>') {
+    myfile.get(ch);
+    if (ch == '>') {
+      myfile.get(ch);
+      if (ch == '=') {
+        // cout << ">>=" << endl;
+        t.type = SymbaolTokan;
+        t.val = ">>=";
+        return t;
+      } else {
+        myfile.putback(ch);
+        // cout << "Cin" << endl;
+        t.type = SymbaolTokan;
+        t.val = ">>";
+        return t;
+      }
+    } else {
+      myfile.putback(ch);
+      // cout << "Grater" << endl;
+      t.type = SymbaolTokan;
+      t.val = ">";
+      return t;
+    }
+  } else if (ch == '<') {
+    myfile.get(ch);
+    if (ch == '<') {
+      myfile.get(ch);
+      if (ch == '=') {
+        // cout << "<<=" << endl;
+        t.type = SymbaolTokan;
+        t.val = "<<=";
+        return t;
+      } else {
+        myfile.putback(ch);
+        // cout << "Cout" << endl;
+        t.type = SymbaolTokan;
+        t.val = "<<";
+        return t;
+      }
+    } else {
+      myfile.putback(ch);
+      // cout << "less" << endl;
+      t.type = SymbaolTokan;
+      t.val = "<";
+      return t;
+    }
+  }
+	return t;
+}
+
+bool T(){
+	Token t;
+	myfile.get(ch);
+	t = Lexical();
+	if(t.type == 2){
+		return true;
+	}
+	else return false;
+}
+
+bool F(){
+	Token t;
+	myfile.get(ch);
+	t = Lexical();
+	if(t.val == "*"){
+		return T();
+	}
+	else return false;
+}
+		
+bool E(){
+	Token t;
+	myfile.get(ch);
+	t = Lexical();
+	if(t.type == 2){
+		return F();
+	}
+	else return false;
+}
+
+int main() {
+  myfile.open("text.txt", ios::in);
+	cout<<E();
 }
